ef2416d4fba134b4821fe2d26ebd669c7ddf7855
fernando <fernandoradoaca@gmail.com>
2024-04-26 00:28:26 +0200

    BackUpAnimations

---
+++ Assets/Scripts/Animation4dController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Animation4dController : MonoBehaviour
+{
+    
+    
+    [SerializeField] private float speedMovement;
+    private Vector2 direction;
+    private Rigidbody2D rb2D;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+
+        // Inicialmente, asignamos una dirección de movimiento aleatoria
+        ChangeDirection();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("MovementX", direction.x);
+        animator.SetFloat("MovementY", direction.y);
+    }
+
+    private void FixedUpdate()
+    {
+        // Movemos el personaje en la dirección actual
+        rb2D.MovePosition(rb2D.position + direction * speedMovement * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Cambiar la dirección hacia la opuesta cuando se detecta una colisión
+        ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        // Generamos una dirección de movimiento aleatoria en ambos ejes
+        float randomX = Random.Range(-1f, 1f);
+        float randomY = Random.Range(-1f, 1f);
+        direction = new Vector2(randomX, randomY).normalized;
+    }
+}
