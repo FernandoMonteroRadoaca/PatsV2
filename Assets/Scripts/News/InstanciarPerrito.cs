61d10cd663a4441e27889c9340e7acb0c71dc038
AngelaOni <acanoonivenis@gmail.com>
2024-05-15 22:16:11 +0200

    perrito acercandose a camara

---
+++ Assets/Scripts/News/InstanciarPerrito.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class InstanciarPerrito : MonoBehaviour
+{
+    public GameObject[] prefabsPerritos; // Asigna los prefabs de los perritos en el Inspector
+
+    void Start()
+    {
+        string nombrePerritoSeleccionado = PlayerPrefs.GetString("PerritoSeleccionado");
+
+        // Busca el índice del perrito seleccionado en el array de prefabs
+        int indicePerritoSeleccionado = -1;
+        for (int i = 0; i < prefabsPerritos.Length; i++)
+        {
+            if (prefabsPerritos[i].name == nombrePerritoSeleccionado)
+            {
+                indicePerritoSeleccionado = i;
+                break;
+            }
+        }
+
+        // Si se encuentra el perrito seleccionado, instancia el prefab correspondiente
+        if (indicePerritoSeleccionado != -1)
+        {
+            GameObject perrito = Instantiate(prefabsPerritos[indicePerritoSeleccionado], transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogWarning("Perrito seleccionado no encontrado o prefab correspondiente no asignado.");
+        }
+    }
+}
---
+++ Assets/Scripts/News/RandomMov.cs
@@ -0,0 +1,145 @@
+using UnityEngine;
+using System.Collections;
+
+public class RandomMov : MonoBehaviour
+{
+    public float velocidadMinima = 2f;
+    public float velocidadMaxima = 5f;
+    public float duracionMovimientoMinima = 2f;
+    public float duracionMovimientoMaxima = 5f;
+    public float duracionPausaMinima = 1f;
+    public float duracionPausaMaxima = 3f;
+
+    public float limiteIzquierda = -8f;
+    public float limiteDerecha = 8f;
+    public float limiteSuelo = -3f;
+    public float limiteAltura = -1f;
+
+    private Vector2 direccionMovimiento;
+    private float velocidadActual;
+    private Animator animator;
+    private bool enEjecucion = true;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(CambiarMovimiento());
+    }
+
+    void Update()
+    {
+        if (!enEjecucion)
+            return;
+
+        Vector3 newPosition = transform.position + (new Vector3(direccionMovimiento.x, direccionMovimiento.y, 0) * velocidadActual * Time.deltaTime);
+
+        if (newPosition.x < limiteIzquierda || newPosition.x > limiteDerecha)
+        {
+            direccionMovimiento.x *= -1;
+            newPosition.x = Mathf.Clamp(newPosition.x, limiteIzquierda, limiteDerecha);
+        }
+
+        if (newPosition.y < limiteSuelo || newPosition.y > limiteAltura)
+        {
+            direccionMovimiento.y *= -1;
+            newPosition.y = Mathf.Clamp(newPosition.y, limiteSuelo, limiteAltura);
+        }
+
+        transform.position = newPosition;
+
+        if (direccionMovimiento != Vector2.zero)
+        {
+            float horizontal = direccionMovimiento.x;
+            float vertical = direccionMovimiento.y;
+
+            if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+            {
+                animator.SetBool("Derecha", horizontal > 0);
+                animator.SetBool("Izquierda", horizontal < 0);
+                animator.SetBool("Arriba", false);
+                animator.SetBool("Abajo", false);
+                animator.SetBool("Parado", false);
+            }
+            else
+            {
+                animator.SetBool("Derecha", false);
+                animator.SetBool("Izquierda", false);
+                animator.SetBool("Arriba", vertical > 0);
+                animator.SetBool("Abajo", vertical < 0);
+                animator.SetBool("Parado", false);
+            }
+        }
+
+        if (velocidadActual == 0)
+        {
+            HacerParada();
+        }
+    }
+
+    IEnumerator CambiarMovimiento()
+    {
+        while (enEjecucion)
+        {
+            direccionMovimiento = GenerateRandomVector();
+            velocidadActual = Random.Range(velocidadMinima, velocidadMaxima);
+
+            yield return new WaitForSeconds(Random.Range(duracionMovimientoMinima, duracionMovimientoMaxima));
+
+            direccionMovimiento = Vector2.zero;
+            velocidadActual = 0;
+
+            yield return new WaitForSeconds(Random.Range(duracionPausaMinima, duracionPausaMaxima));
+        }
+    }
+
+    public void HacerParada()
+    {
+        animator.SetBool("Derecha", false);
+        animator.SetBool("Izquierda", false);
+        animator.SetBool("Arriba", false);
+        animator.SetBool("Abajo", false);
+        animator.SetBool("Parado", true);
+    }
+
+    public void detenerScript()
+    {
+        enEjecucion = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Perro"))
+        {
+            // Obtener la dirección del choque
+            Vector2 normal = collision.GetContact(0).normal;
+
+            // Reflejar la dirección del movimiento
+            direccionMovimiento = Vector2.Reflect(direccionMovimiento, normal);
+        }
+    }
+
+    private Vector2 GenerateRandomVector()
+    {
+        float x, y;
+
+        float probability = Random.value;
+
+        if (probability <= 0.9f)
+        {
+            x = Random.Range(-2.5f, 2.5f);
+            y = Random.Range(-0.5f, 0.5f);
+        }
+        else
+        {
+            y = Random.Range(-2.5f, 2.5f);
+            x = Random.Range(-0.5f, 0.5f);
+        }
+
+        Vector2 normalizedVector = new Vector2(x, y).normalized;
+
+        return normalizedVector;
+    }
+}
